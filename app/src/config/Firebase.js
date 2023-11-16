18fdffa6b452aaf936ca040e9289b524976dba26
anshumancodes <anshumanprof01@gmail.com>
2023-11-16 18:15:52 +0530

    intial commit

---
+++ app/src/config/Firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+// Import the Firebase configuration from api.js
+import firebaseConfig from './api';
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); // using this as avariable for secuirty 
+const analytics = getAnalytics(app);
