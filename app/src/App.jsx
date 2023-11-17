b1b641dba9f3074dd26b2774bf4e5e696f4143c9
anshumancodes <anshumanprof01@gmail.com>
2023-11-17 11:33:59 +0530

    modified - completed headed component

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,25 @@
-
-
-import './App.css'
-
+import React from "react"
+import Nav from "./components/Nav"
+import { useState } from "react";
 function App() {
- 
+  const [isDarkMode, setDarkMode] = useState(false);
+  const toggleMode = () => {
+    setDarkMode(!isDarkMode);
+  };
+  
+  const BackgroundMode={
+    backgroundColor: isDarkMode ? 'whiteColorBg' : 'primary-black-bg'  ,
+     
+   }
 
+  
+   
   return (
-    <>
-     <div>hello world!</div>
-    </>
+    
+    <div className={`h-screen bg-${BackgroundMode.backgroundColor}`}>
+      <Nav isDarkMode={isDarkMode} toggleMode={toggleMode} />
+      
+    </div>
   )
 }
 
