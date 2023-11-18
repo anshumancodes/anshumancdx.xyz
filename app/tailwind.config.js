6a91ae3c7add9053e20963fe26d05869deaa2883
anshumancodes <anshumanprof01@gmail.com>
2023-11-18 16:18:51 +0530

    modified to make changes with custom colors and added custom width

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,25 +1,24 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+const tailwindconfig = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+ 
   theme: {
     extend: {
-      colors: {
-        'primary-black-bg':'#0d1117',
-        'whiteColorBg': '#e1e3f9',
-        'purple-color': '#acb4fe',
-        'purple-smoke-color': '#cdd0f3',
-        'gray-color': '#161b22',
-        'white-smoke-color': '#8b949e',
+      colors:{
+        'black-bg': '#0d1117',
         'white-color': '#e5e5e5',
-        'gradient-color': 'linear-gradient(99.51deg, #cbbee7 9.03%, #fff 89.53%)',
-        'white-gradient-color': 'linear-gradient(99.51deg, #acb4fe 9.03%, #ae88fe 89.53%)',
+        // 
+        'white-color-bg':'#e1e3f9',
+        'purple-dark-color':'#ae88fe',
+        'purple-smoke':'#cdd0f3',
+        'gray-color':'#161b22'
+      }, width: {
+        '80c': '80%',
       },
+     
     },
-    
   },
   plugins: [],
-}
+};
 
+export default tailwindconfig;
