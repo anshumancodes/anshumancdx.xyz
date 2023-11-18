0b5d7884c84790212e22f91b7bd9b4a5e0e1f3e1
anshumancodes <anshumanprof01@gmail.com>
2023-11-19 01:03:44 +0530

    added new component Introduction.jsx

---
+++ app/src/components/Introduction.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Introduction = ({isDarkMode}) => {
+  return (
+    <div className="justify-center   flex w-full mt-5gen">
+      <div className="w-40c  flex-col flex gap-4  text-left">
+
+        <span className={`flex ${isDarkMode ? "darktxt" : "lighttxt"}`} >
+          <span className="text-purple-color-normal">Anshuman🍕</span>
+          -Introduction
+        </span>
+
+        <p className="text-white-smoke-color">
+          Hello, this is Anshuman and I love computer science , I have been writing code and creating small web apps since a year now.
+          am familiar with few programming languages like JavaScript , python and C
+        </p>
+
+        <span>
+            <ul>
+                <li>-Web developer</li>
+                <li>-Designer </li>
+            </ul>
+        </span>
+        <div className="w-full flex justify-center"><img src="./imgs/pizza.png" alt="" className="w-200pwx rounded-complete"/></div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduction;
