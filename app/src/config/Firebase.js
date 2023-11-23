2ed3c0eb0030abe406a8b2512b7fa7ff60e4f60d
anshumancodes <anshumanprof01@gmail.com>
2023-11-23 22:44:28 +0530

    modified firebase config

--- app/src/config/Firebase.js
+++ app/src/config/Firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -14,5 +15,6 @@ import firebaseConfig from './api';
 
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig); // using this as avariable for secuirty 
+const app = initializeApp(firebaseConfig); // using this as a variable for secuirty 
 const analytics = getAnalytics(app);
+export const db = getFirestore(app);
