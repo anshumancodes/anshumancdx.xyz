caf6053356c7cc9f921baf58495649a2c9a38687
anshumancodes <anshumanprof01@gmail.com>
2023-11-25 19:39:24 +0530

    created footer

---
+++ app/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='flex w-screen justify-center'>
+      <footer className='text-white-smoke-color text-1x mt-5gen mb-5'>anshumancdx © 2023</footer>
+    </div>
+  )
+}
+
+export default Footer
