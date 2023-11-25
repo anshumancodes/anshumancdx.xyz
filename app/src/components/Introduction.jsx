f6be2af94c0f757207e8babfca42462bcc0dcdd5
anshumancodes <anshumanprof01@gmail.com>
2023-11-25 19:37:24 +0530

    modified introduction

--- app/src/components/Introduction.jsx
+++ app/src/components/Introduction.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Introduction = ({isDarkMode}) => {
   return (
     <div className="justify-center   flex w-full mt-5gen">
-      <div className="w-40c  flex-col flex gap-4  text-left">
+      <div className="w-40c max-[594px]:w-70c ]  flex-col flex gap-4  text-left">
 
         <span className={`flex ${isDarkMode ? "darktxt" : "lighttxt"}`} >
           <span className="text-purple-color-normal">Anshuman🍕</span>
@@ -12,7 +12,7 @@ const Introduction = ({isDarkMode}) => {
 
         <p className="text-white-smoke-color">
           Hello, this is Anshuman and I love computer science , I have been writing code and creating small web apps since a year now.
-          am familiar with few programming languages like JavaScript , python and C
+          am familiar with few programming languages like JavaScript , python and C & right now am a fulltime comp sci student.
         </p>
 
         <span>
