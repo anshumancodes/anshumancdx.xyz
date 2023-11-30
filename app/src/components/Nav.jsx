c9c2be3171b2f11da70cf8fb7017f854e4a9715c
anshumancodes <anshumanprof01@gmail.com>
2023-11-30 20:26:22 +0530

    modified

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -18,12 +18,12 @@ const Nav = ({ isDarkMode, toggleMode }) => {
     <header className="w-full flex justify-center">
     <nav className="flex flex-wrap justify-between items-center w-80c py-5">
       <span className={`text-${textMode.color} flex gap-8 cursor-crosshair font-semibold text-lg items-center`} >
-        <a href="" onClick={Notavailableyet}>Works</a>
+        <a href="#" onClick={Notavailableyet}>Works</a>
         <a href="https://github.com/anshumancodes" className="flex gap-1 items-center">Source <DiGithubBadge className="text-3xl"/></a>
         <a href="/blogs">blogs</a>
       </span>
 
-      <span className=""><button className={` ${isDarkMode ? "darkbtnbg" : "lightbtnbg"} p-3 text-${textMode.color} rounded-lg`} onClick={toggleMode}>{isDarkMode ? <MdLightMode  /> : <MdDarkMode />}</button></span>
+      <span className=""><button className={` ${isDarkMode ? "darkbtnbg" : "lightbtnbg"} p-3 text-${textMode.color} rounded-lg`} onClick={toggleMode} name="mode-toggle-button"  >{isDarkMode ? <MdLightMode  /> : <MdDarkMode />}</button></span>
     </nav>
     </header>
   );
