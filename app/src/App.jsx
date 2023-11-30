069e7191efc577123a1f345f6989cbfe44760ce3
anshumancodes <anshumanprof01@gmail.com>
2023-11-30 20:27:34 +0530

    modified

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,20 +1,11 @@
-import React, { useState, lazy } from "react";
+import React, { useState, lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
-
-// import Blogs from "./components/Blogs";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
 const Home = lazy(() => import("./components/Home"));
 const Addblogs = lazy(() => import("./components/Addblogs"));
 const Blog = lazy(() => import("./components/Blog"));
 const Adminlogin = lazy(() => import("./components/Adminlogin"));
-const Blogs=lazy(()=> import ("./components/Blogs") );
-
-
+const Blogs = lazy(() => import("./components/Blogs"));
 
 function App() {
   const [isDarkMode, setDarkMode] = useState(true);
@@ -33,45 +24,33 @@ function App() {
   return (
     <div>
       <Router>
-        <Routes>
-          <Route
-            path="/"
-            element={<Home isDarkMode={isDarkMode} toggleMode={toggleMode} />}
-          />
-          <Route
-            path="/blogs"
-            element={<Blogs isDarkMode={isDarkMode} toggleMode={toggleMode} />}
-          />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Home isDarkMode={isDarkMode} toggleMode={toggleMode} />} />
+            <Route path="/blogs" element={<Blogs isDarkMode={isDarkMode} toggleMode={toggleMode} />} />
+            <Route path="/blog/:id" element={<Blog isDarkMode={isDarkMode} toggleMode={toggleMode} />} />
 
-          <Route
-            path="/blog/:id"
-            element={<Blog isDarkMode={isDarkMode} toggleMode={toggleMode} />}
-          />
-          
-          {/* Routes for admin */}
-          <Route
-            path="/admin/*"
-            element={
-              isAdminLoggedIn ? (
-                // Render the admin routes only if logged in
-                <Routes>
-                  <Route path="/createblog" element={<Addblogs />} />
-                </Routes>
-              ) : (
-                // Redirect to admin login if not logged in
-                <Navigate to="/admin/login" />
-              )
-            }
-          />
-          <Route
-            path="/admin/login"
-            element={<Adminlogin onLogin={handleAdminLogin} />}
-          />
-        </Routes>
+            {/* Routes for admin */}
+            <Route
+              path="/admin/*"
+              element={
+                isAdminLoggedIn ? (
+                  // Render the admin routes only if logged in
+                  <Routes>
+                    <Route path="/createblog" element={<Addblogs />} />
+                  </Routes>
+                ) : (
+                  // Redirect to admin login if not logged in
+                  <Navigate to="/admin/login" />
+                )
+              }
+            />
+            <Route path="/admin/login" element={<Adminlogin onLogin={handleAdminLogin} />} />
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   );
 }
 
 export default App;
-
