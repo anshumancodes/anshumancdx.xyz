048896733022e4b59508e152657a21c9765bd37c
anshumancodes <anshumanprof01@gmail.com>
2023-11-30 12:59:48 +0530

    added admin acess link

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='flex w-screen justify-center'>
+    <div className='flex w-screen items-center justify-center gap-3 flex-col'>
       <footer className='text-white-smoke-color text-1x mt-5gen mb-5'>anshumancdx © 2023</footer>
+      <a href="/admin" className='mb-5 text-blue-600'>login as admin</a>
     </div>
   )
 }
