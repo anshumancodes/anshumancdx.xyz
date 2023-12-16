9cc8d0b1f756f8919998ced31895ab060f47031e
anshumancodes <anshumanprof01@gmail.com>
2023-12-16 19:00:51 +0530

    modified
    modified the card style

--- app/src/components/Blogs.jsx
+++ app/src/components/Blogs.jsx
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
+
+import React, { useState, useEffect } from "react";
 import Nav from "./Nav";
-// import fb from "../firebase";
-import { useEffect } from "react";
 import { collection, doc, getDocs } from "firebase/firestore";
 import { db } from "../config/Firebase";
 import { Link } from "react-router-dom";
+import { IoIosArrowRoundForward } from "react-icons/io";
 
+// ... (previous imports)
 
 const Blogs = ({ isDarkMode, toggleMode }) => {
-  const [blogs, setblog] = useState();
-  //   const Blogs=collection(db,'blogs');
+  const [blogs, setBlog] = useState();
 
   useEffect(() => {
     const fetchBlogs = async () => {
@@ -22,9 +22,7 @@ const Blogs = ({ isDarkMode, toggleMode }) => {
             ...doc.data(),
           };
         });
-        setblog(blogsList);
-
-        
+        setBlog(blogsList);
       } catch (error) {
         console.error("Error fetching blogs:", error);
       }
@@ -32,45 +30,61 @@ const Blogs = ({ isDarkMode, toggleMode }) => {
 
     fetchBlogs();
   }, []);
-// for split the blog content
-
-
 
   const maxLength = 80;
 
   return (
-    <div className={`h-fit ${isDarkMode ? "dark" : "light"} flex flex-col` }>
-      <Nav toggleMode={toggleMode} isDarkMode={isDarkMode}/>
-      <div >
-      {blogs ? (
-  blogs.length > 0 ? (
-    blogs.map((blog) => (
-      <div
-        key={blog.id}
-        className="flex gap-4 w-full px-8 py-3 mb-6 justify-center"
-      >
-        {/*  */}
-
-        <div className={`${isDarkMode ? "darkbtnbg" : "lightbtnbg"} flex flex-col w-80c p-3 rounded-medium-card`}>
-          <span className="flex gap-2 Large:flex   flex-wrap  justify-between" ><h2 className="  Large:font-bold text-1x+">{blog.title}</h2> <p>{blog.date}</p> </span>
-         <p>{blog.intro.split(" ").slice(0,80).join(" ") }<Link to={`/blog/${blog.id}`} className="text-blue-500">read more</Link></p>
-
-          
-        
-        </div>
-
-        {/*  */}
-
-
-      </div>
-    ))
-  ) : (
-    <p className="w-full flex justify-center items-center h-full">No blogs available</p>
-  )
-) : (
-  <p className="w-full flex justify-center items-center ">loading...</p>
-)}
-
+    <div className={`h-fit ${isDarkMode ? "dark" : "light"} flex flex-col`}>
+      <Nav toggleMode={toggleMode} isDarkMode={isDarkMode} />
+      <div className="mt-10up grid gap-8 lg:grid-cols-2 ml-4 mr-4">
+        {blogs ? (
+          blogs.length > 0 ? (
+            blogs.map((blog) => (
+              <article
+                key={blog.id}
+                className="flex flex-col gap-4 w-full mb-6 justify-center lg:w-auto"
+              >
+                <div
+                  className={`${
+                    isDarkMode ? "darkbtnbg" : "light border-[2px]"
+                  } flex flex-wrap gap-4 p-3 rounded-basic h-full`}
+                >
+                  {/* <div>
+                    <img
+                      src={blog.primaryimg}
+                      alt={blog.title}
+                      className="w-300pwx h-40  rounded-none lg:w-200pwx lg:w-96 lg:h-52 object-cover lg:rounded-full"
+                    />
+                  </div> */}
+                  <div className="flex flex-col gap-4 w-full">
+                    <span className="flex gap-2 Large:flex flex-wrap justify-between items-center">
+                      <h2 className="Large:font-bold text-1x+">
+                        {blog.title}
+                      </h2>
+                      <p className="text-sm text-gray-400">{blog.date}</p>
+                    </span>
+                    <p className="w-full Large:w-96">
+                      {blog.intro.split(" ").slice(0, maxLength).join(" ")}
+                      <Link
+                        to={`/blog/${blog.id}`}
+                        className="text-blue-500 ml-1 flex items-center"
+                      >
+                        read more
+                        <IoIosArrowRoundForward className="text-2x" />
+                      </Link>
+                    </p>
+                  </div>
+                </div>
+              </article>
+            ))
+          ) : (
+            <p className="w-full flex justify-center items-center h-full">
+              No blogs available
+            </p>
+          )
+        ) : (
+          <p className="w-full flex justify-center items-center">loading...</p>
+        )}
       </div>
     </div>
   );
