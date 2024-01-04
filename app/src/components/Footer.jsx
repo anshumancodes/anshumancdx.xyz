5ffcde1b91b83e51c30c1699be49d14b4427616f
anshumancodes <anshumanprof01@gmail.com>
2024-01-04 23:15:28 +0530

    updated copyright year

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='flex w-screen items-center justify-center gap-3 flex-col'>
-      <footer className='text-white-smoke-color text-1x mt-5gen mb-5'>anshumancdx © 2023</footer>
+      <footer className='text-white-smoke-color text-1x mt-5gen mb-5'>anshumancdx © {new Date ().getFullYear()}</footer>
       <a href="/admin" className='mb-5 text-blue-600'>login as admin</a>
     </div>
   )
