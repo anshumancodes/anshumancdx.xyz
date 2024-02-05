5268c2ee1d7e4bf833e9d80c244484a1c95a93b6
anshumancodes <anshumanprof01@gmail.com>
2024-02-05 23:58:48 +0530

    modified

---
+++ app/src/components/Hireme.jsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import Nav from './Nav'
+
+const Hireme = ({ isDarkMode, toggleMode }) => {
+  return (
+    <div className='h-fit bg-black-bg'>
+      <div class="overflow-x-hidden">
+
+
+  <div class="relative py-12 sm:py-16 lg:pt-20 xl:pb-0">
+    <div class="relative mx-auto hidden max-w-screen-lg md:block">
+
+   
+
+      
+    </div>
+    <div class="relative mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="mx-auto max-w-3xl text-center">
+        <h1 class="mt-5 text-3xl font-light leading-snug text-white sm:text-5xl sm:leading-snug lg:text-6xl lg:leading-snug">
+        Create Your Business-Ready Web Apps Quickly,  <br class="sm:hidden" />
+        
+           
+
+             
+          <span className='text-blue-500'> Tailored Just for You ,by Me</span>
+        </h1>
+        <p class="mx-auto mt-10 max-w-md text-base leading-7 text-gray-300">Boost Your App Instantly, Launch Quicker, Start Earning </p>
+
+        <form action="#" method="POST" class="mt-10">
+          <div class="group bg-white/20 border-white/50 focus-within:ring-1 focus-within:ring-white/50 relative border p-1 sm:flex-row">
+            <input type="email" name="" placeholder="Enter email address" class="placeholder-white/50 block w-full bg-transparent px-4 py-4 text-white outline-none" required="" />
+            <div class="border-white/50 flex sm:absolute sm:inset-y-0 sm:right-0 sm:h-full sm:border-l">
+              <button type="submit" class="inline-flex w-full items-center justify-center bg-blue-600 px-6 py-3 text-lg font-bold text-white outline-none transition-all focus:bg-blue-500 hover:border sm:hover:translate-x-2 sm:hover:-translate-y-2">Book a call now</button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+
+    <div class="w-fit mx-auto mt-16 mb-16 flex flex-col justify-center divide-y divide-gray-600 sm:flex-row sm:divide-x sm:divide-y-0 md:mt-20">
+      <div class="bg-gray-700 px-6 py-3">
+        <p class="flex items-center space-x-3 text-2xl font-bold">
+         
+          <span class="w-8"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14"><path stroke="#2f77ff" stroke-linecap="round" stroke-linejoin="round" d="M13.2463 3.51912C13.2463 4.45527 12.9424 5.35308 12.4014 6.01504C11.9745 6.53735 11.4277 6.87906 10.8402 7L3.1533 6.99798C2.56945 6.87529 2.02629 6.53447 1.6018 6.01504C1.06083 5.35308 0.756917 4.45527 0.756917 3.51912L0.7569 2.02218C0.756897 1.83787 0.877987 1.67545 1.05463 1.62284C4.93503 0.467077 9.06813 0.46708 12.9485 1.62285C13.1252 1.67546 13.2463 1.83787 13.2463 2.02218V3.51912Z"></path><path stroke="#2f77ff" stroke-linecap="round" stroke-linejoin="round" d="M6.99622 10.2227V13.25"></path><path stroke="#2f77ff" stroke-linecap="round" stroke-linejoin="round" d="M4.59241 13.25H9.4001"></path><path fill="#D7E0FF" stroke="#2f77ff" d="M3.52159 6.59555C3.58217 8.50666 5.08952 10.2151 7.0016 10.2151C8.94734 10.2151 10.4129 8.53676 10.4799 6.59217C10.4925 6.22608 10.4993 5.85621 10.4993 5.48257C10.4993 3.96735 10.3982 2.4241 10.2328 1.05536C9.19364 0.823367 8.12497 0.75 7.0016 0.75C5.87822 0.75 4.78799 0.814457 3.77037 1.05536C3.59603 2.41834 3.50391 3.96735 3.50391 5.48257C3.50391 5.85749 3.50995 6.22845 3.52159 6.59555Z"></path></svg></span>
+          <span class="text-gray-100">500+</span>
+        </p>
+        <p class="text-sm text-gray-400">Achievements & Awards</p>
+      </div>
+      <div class="bg-gray-700 px-6 py-3">
+        <p class="flex items-center space-x-3 text-2xl font-bold">
+         
+          <span class="w-8"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14"><path fill="#D7E0FF" stroke="#2f77ff" stroke-linecap="round" stroke-linejoin="round" d="M7 13.25C10.4518 13.25 13.25 10.4518 13.25 7C13.25 3.54822 10.4518 0.75 7 0.75C3.54822 0.75 0.75 3.54822 0.75 7C0.75 10.4518 3.54822 13.25 7 13.25Z"></path><path stroke="#2f77ff" stroke-linecap="round" stroke-linejoin="round" d="M3.8269 7.96143C4.30767 9.69219 6.23075 10.7499 7.96152 10.2691C9.01921 9.8845 9.88459 9.01912 10.1731 7.96143"></path><path stroke="#2f77ff" stroke-linecap="round" d="M4.45193 5.10068L4.45193 5.80068"></path><path stroke="#2f77ff" stroke-linecap="round" d="M9.54813 5.10068L9.54813 5.80068"></path></svg></span>
+          <span class="text-gray-100">100%</span>
+        </p>
+        <p class="text-sm text-gray-400">Happy Clients in 10 years</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+{/*  */}
+<div className='flex flex-col items-center mt-[200px]'>
+<img src="/imgs/download.png" alt=""  className='lg:w-[500px]'/>
+    
+</div>
+
+      
+    </div>
+  )
+}
+
+export default Hireme
