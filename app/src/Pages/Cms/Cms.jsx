499c93b520a5b4c3517a6dd49fd858fd3bd453ee
anshumancodes <anshumanprof01@gmail.com>
2024-10-02 00:44:17 +0530

    add layout changes to cms

--- app/src/Pages/Cms/Cms.jsx
+++ app/src/Pages/Cms/Cms.jsx
@@ -1,18 +1,33 @@
-import React ,{lazy} from 'react'
-import Sidebar from '../../components/cms/Sidebar'
+import React, { lazy, useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Sidebar from '../../components/cms/Sidebar';
+import ManageBlogs from '../../components/blog/ManageBlogs';
+import BlogEditor from '../../components/blog/BlogEditor';
 
 const Cms = () => {
+  const [manage, setManage] = useState(false);
+  const location = useLocation();
+
+  // Set manage to true when the path is '/cms/manage'
+  useEffect(() => {
+    if (location.pathname === '/cms/manage') {
+      setManage(true);
+    } else {
+      setManage(false);
+    }
+  }, [location.pathname]);
+
   return (
-    <div className='flex flex-row gap-10'>
-      <div className='absolute left-0 h-full'>
-        <span>
-            <a href="/">Home</a>
-        </span>
+    <div className="flex flex-row gap-10">
+      <div className="left-0">
         <Sidebar />
       </div>
-      
+      <div className="flex-1">
+        {manage ? <ManageBlogs /> : <BlogEditor />}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Cms
+export default Cms;
+
