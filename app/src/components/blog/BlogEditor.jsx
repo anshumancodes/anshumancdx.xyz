3303077c1b4fdcf2a50bc0b76f0a0632631ba408
anshumancodes <anshumanprof01@gmail.com>
2024-10-03 01:40:43 +0530

    add function to write into firebase firestore db

--- app/src/components/blog/BlogEditor.jsx
+++ app/src/components/blog/BlogEditor.jsx
@@ -1,80 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
-
-import {
-  ClassicEditor,
-  AccessibilityHelp,
-  Alignment,
-  Autoformat,
-  AutoImage,
-  AutoLink,
-  Autosave,
-  BlockQuote,
-  BlockToolbar,
-  Bold,
-  CloudServices,
-  Code,
-  CodeBlock,
-  Essentials,
-  FindAndReplace,
-  FontBackgroundColor,
-  FontColor,
-  FontFamily,
-  FontSize,
-  FullPage,
-  GeneralHtmlSupport,
-  Heading,
-  Highlight,
-  HorizontalLine,
-  HtmlComment,
-  HtmlEmbed,
-  ImageBlock,
-  ImageCaption,
-  ImageInline,
-  ImageInsertViaUrl,
-  ImageResize,
-  ImageStyle,
-  ImageTextAlternative,
-  ImageToolbar,
-  ImageUpload,
-  Indent,
-  IndentBlock,
-  Italic,
-  Link,
-  LinkImage,
-  List,
-  ListProperties,
-  Markdown,
-  MediaEmbed,
-  Mention,
-  Paragraph,
-  PasteFromMarkdownExperimental,
-  PasteFromOffice,
-  RemoveFormat,
-  SelectAll,
-  ShowBlocks,
-  SourceEditing,
-  SpecialCharacters,
-  SpecialCharactersArrows,
-  SpecialCharactersCurrency,
-  SpecialCharactersEssentials,
-  SpecialCharactersLatin,
-  SpecialCharactersMathematical,
-  SpecialCharactersText,
-  Strikethrough,
-  Subscript,
-  Superscript,
-  Table,
-  TableCaption,
-  TableCellProperties,
-  TableColumnResize,
-  TableProperties,
-  TableToolbar,
-  TextTransformation,
-  TodoList,
-  Underline,
-  Undo,
-} from "ckeditor5";
+import editorConfig from "../../config/BlogeditorConfig";
+import { ClassicEditor } from "ckeditor5";
+import { db } from '../../config/Firebase';
+import { collection, doc, setDoc } from "firebase/firestore";
 
 import "ckeditor5/ckeditor5.css";
 
@@ -82,6 +11,11 @@ const BlogEditor = () => {
   const editorContainerRef = useRef(null);
   const editorRef = useRef(null);
   const [isLayoutReady, setIsLayoutReady] = useState(false);
+  const [content, setContent] = useState("");
+  const [isDraft, setDraft] = useState(false);
+  const [errorCreating, setErrorCreating] = useState(false);
+  const [createdSuccess, setCreatedSuccess] = useState(false);
+  const [slug, setSlug] = useState("");
 
   useEffect(() => {
     setIsLayoutReady(true);
@@ -89,282 +23,91 @@ const BlogEditor = () => {
     return () => setIsLayoutReady(false);
   }, []);
 
-  const editorConfig = {
-    toolbar: {
-      items: [
-        "undo",
-        "redo",
-        "|",
-        "sourceEditing",
-        "showBlocks",
-        "findAndReplace",
-        "|",
-        "heading",
-        "|",
-        "fontSize",
-        "fontFamily",
-        "fontColor",
-        "fontBackgroundColor",
-        "|",
-        "bold",
-        "italic",
-        "underline",
-        "strikethrough",
-        "subscript",
-        "superscript",
-        "code",
-        "removeFormat",
-        "|",
-        "specialCharacters",
-        "horizontalLine",
-        "link",
-        "insertImageViaUrl",
-        "mediaEmbed",
-        "insertTable",
-        "highlight",
-        "blockQuote",
-        "codeBlock",
-        "htmlEmbed",
-        "|",
-        "alignment",
-        "|",
-        "bulletedList",
-        "numberedList",
-        "todoList",
-        "outdent",
-        "indent",
-      ],
-      shouldNotGroupWhenFull: false,
-    },
-    plugins: [
-      AccessibilityHelp,
-      Alignment,
-      Autoformat,
-      AutoImage,
-      AutoLink,
-      Autosave,
-      BlockQuote,
-      BlockToolbar,
-      Bold,
-      CloudServices,
-      Code,
-      CodeBlock,
-      Essentials,
-      FindAndReplace,
-      FontBackgroundColor,
-      FontColor,
-      FontFamily,
-      FontSize,
-      FullPage,
-      GeneralHtmlSupport,
-      Heading,
-      Highlight,
-      HorizontalLine,
-      HtmlComment,
-      HtmlEmbed,
-      ImageBlock,
-      ImageCaption,
-      ImageInline,
-      ImageInsertViaUrl,
-      ImageResize,
-      ImageStyle,
-      ImageTextAlternative,
-      ImageToolbar,
-      ImageUpload,
-      Indent,
-      IndentBlock,
-      Italic,
-      Link,
-      LinkImage,
-      List,
-      ListProperties,
-      Markdown,
-      MediaEmbed,
-      Mention,
-      Paragraph,
-      PasteFromMarkdownExperimental,
-      PasteFromOffice,
-      RemoveFormat,
-      SelectAll,
-      ShowBlocks,
-      SourceEditing,
-      SpecialCharacters,
-      SpecialCharactersArrows,
-      SpecialCharactersCurrency,
-      SpecialCharactersEssentials,
-      SpecialCharactersLatin,
-      SpecialCharactersMathematical,
-      SpecialCharactersText,
-      Strikethrough,
-      Subscript,
-      Superscript,
-      Table,
-      TableCaption,
-      TableCellProperties,
-      TableColumnResize,
-      TableProperties,
-      TableToolbar,
-      TextTransformation,
-      TodoList,
-      Underline,
-      Undo,
-    ],
-    blockToolbar: [
-      "fontSize",
-      "fontColor",
-      "fontBackgroundColor",
-      "|",
-      "bold",
-      "italic",
-      "|",
-      "link",
-      "insertTable",
-      "|",
-      "bulletedList",
-      "numberedList",
-      "outdent",
-      "indent",
-    ],
-    fontFamily: {
-      supportAllValues: true,
-    },
-    fontSize: {
-      options: [10, 12, 14, "default", 18, 20, 22],
-      supportAllValues: true,
-    },
-    heading: {
-      options: [
-        {
-          model: "paragraph",
-          title: "Paragraph",
-          class: "ck-heading_paragraph",
-        },
-        {
-          model: "heading1",
-          view: "h1",
-          title: "Heading 1",
-          class: "ck-heading_heading1",
-        },
-        {
-          model: "heading2",
-          view: "h2",
-          title: "Heading 2",
-          class: "ck-heading_heading2",
-        },
-        {
-          model: "heading3",
-          view: "h3",
-          title: "Heading 3",
-          class: "ck-heading_heading3",
-        },
-        {
-          model: "heading4",
-          view: "h4",
-          title: "Heading 4",
-          class: "ck-heading_heading4",
-        },
-        {
-          model: "heading5",
-          view: "h5",
-          title: "Heading 5",
-          class: "ck-heading_heading5",
-        },
-        {
-          model: "heading6",
-          view: "h6",
-          title: "Heading 6",
-          class: "ck-heading_heading6",
-        },
-      ],
-    },
-    htmlSupport: {
-      allow: [
-        {
-          name: /^.*$/,
-          styles: true,
-          attributes: true,
-          classes: true,
-        },
-      ],
-    },
-    image: {
-      toolbar: [
-        "toggleImageCaption",
-        "imageTextAlternative",
-        "|",
-        "imageStyle:inline",
-        "imageStyle:wrapText",
-        "imageStyle:breakText",
-        "|",
-        "resizeImage",
-      ],
-    },
-    initialData:
-      '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n    You\'ve successfully created a CKEditor 5 project. This powerful text editor will enhance your application, enabling rich text editing\n    capabilities that are customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n    <li>\n        <strong>Integrate into your app</strong>: time to bring the editing into your application. Take the code you created and add to your\n        application.\n    </li>\n    <li>\n        <strong>Explore features:</strong> Experiment with different plugins and toolbar options to discover what works best for your needs.\n    </li>\n    <li>\n        <strong>Customize your editor:</strong> Tailor the editor\'s configuration to match your application\'s style and requirements. Or even\n        write your plugin!\n    </li>\n</ol>\n<p>\n    Keep experimenting, and don\'t hesitate to push the boundaries of what you can achieve with CKEditor 5. Your feedback is invaluable to us\n    as we strive to improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n    <li>📝 <a href="https://orders.ckeditor.com/trial/premium-features">Trial sign up</a>,</li>\n    <li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n    <li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n    <li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n    <li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n    See this text, but the editor is not starting up? Check the browser\'s console for clues and guidance. It may be related to an incorrect\n    license key if you use premium features or another feature-related requirement. If you cannot make it work, file a GitHub issue, and we\n    will help as soon as possible!\n</p>\n',
-    link: {
-      addTargetToExternalLinks: true,
-      defaultProtocol: "https://",
-      decorators: {
-        toggleDownloadable: {
-          mode: "manual",
-          label: "Downloadable",
-          attributes: {
-            download: "file",
-          },
-        },
-      },
-    },
-    list: {
-      properties: {
-        styles: true,
-        startIndex: true,
-        reversed: true,
-      },
-    },
-    mention: {
-      feeds: [
-        {
-          marker: "@",
-          feed: [
-           
-          ],
-        },
-      ],
-    },
-    menuBar: {
-      isVisible: true,
-    },
-    placeholder: "Type or paste your content here!",
-    table: {
-      contentToolbar: [
-        "tableColumn",
-        "tableRow",
-        "mergeTableCells",
-        "tableProperties",
-        "tableCellProperties",
-      ],
-    },
+  const blogsRef = collection(db, "blogs");
+
+  const createBlog = async () => {
+    try {
+      const blogData = {
+        content,
+        isDraft: isDraft,
+      };
+
+      const blogDocRef = doc(blogsRef, slug || undefined);
+
+      await setDoc(blogDocRef, blogData);
+
+      console.log("Document written with ID: ", blogDocRef.id);
+      setCreatedSuccess(true);
+      setErrorCreating(false);
+    } catch (error) {
+      console.error("Error creating document: ", error);
+      setErrorCreating(true);
+      setCreatedSuccess(false);
+    }
   };
+
+  // Handle slug input change
+  const handleSlugChange = (event) => {
+    setSlug(event.target.value);
+  };
+
   return (
-    <div>
-       
+    <div className="flex flex-col justify-center items-center">
+      <div className="mt-10">
+        {isDraft ? (
+          <span className="bg-gray-500 mt-5 px-4 py-2">
+            Successfully created an article document! (draft)
+          </span>
+        ) : errorCreating ? (
+          <span className="bg-red-500 mt-5 px-4 py-2">
+            Unable to create an article document!
+          </span>
+        ) : (
+          createdSuccess && (
+            <span className="bg-green-500 mt-5 px-4 py-2">
+              Successfully created an article document!
+            </span>
+          )
+        )}
+      </div>
+
       <div className="main-container font-lato w-fit mx-auto">
-      <input type="text" placeholder="  write slug" className=" ml-[42px] mt-5 mb-5 py-2 px-5 min-w-[795px] max-w-[795px] border border-slate-400 outline-none"/>
+        <input
+          type="text"
+          placeholder="Write slug"
+          className="ml-[42px] mt-5 mb-5 py-2 px-5 min-w-[795px] max-w-[795px] border border-slate-400 outline-none"
+          onChange={handleSlugChange}
+        />
         <div
-          className="editor-container editor-container_classic-editor editor-container_include-block-toolbar min-w-[795px] max-w-[795px] ml-[42px] "
+          className="editor-container editor-container_classic-editor editor-container_include-block-toolbar min-w-[795px] max-w-[795px] ml-[42px]"
           ref={editorContainerRef}
         >
-          <div className="editor-container__editor min-w-[795px] max-w-[795px]  ">
+          <div className="editor-container__editor min-w-[795px] max-w-[795px]">
             <div ref={editorRef}>
               {isLayoutReady && (
-                <CKEditor editor={ClassicEditor} config={editorConfig} />
+                <CKEditor
+                  editor={ClassicEditor}
+                  config={editorConfig}
+                  onChange={(event, editor) => setContent(editor.getData())}
+                />
               )}
             </div>
           </div>
         </div>
-        <span><button className="px-4 py-2 bg-green-500 ml-[42px] mt-5">Create blog</button></span>
+        <span>
+          <button className="px-4 py-2 bg-green-500 ml-[42px] mt-5" onClick={createBlog}>
+            Create blog
+          </button>
+        </span>
+        <span>
+          <button
+            className="px-4 py-2 bg-gray-400 ml-[42px] mt-5"
+            onClick={() => setDraft(true)}
+          >
+            Add to draft
+          </button>
+        </span>
       </div>
-     
     </div>
   );
 };
