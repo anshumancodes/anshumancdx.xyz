fbbd6fd86945f3e286c48be925f4fef5477dfab4
anshumancodes <anshumanprof01@gmail.com>
2024-10-17 17:58:25 +0530

    update Blogs.jsx
    modified fetch blogs function , imports and blogpost mapping , styles

--- app/src/components/blog/Blogs.jsx
+++ app/src/components/blog/Blogs.jsx
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from "react";
 import Nav from "../Nav";
-import { collection, doc, getDocs, query, orderBy } from "firebase/firestore";
-import { db } from "../../config/Firebase";
-import { Link } from "react-router-dom";
-import { IoIosArrowRoundForward } from "react-icons/io";
-import Footer from "../Footer";
-import { HiOutlineArrowSmallRight } from "react-icons/hi2";
 
-// ... (previous imports)
+import { db, cms_db_id, blog_collection_id,ID } from "../../config/appwriteconfig";
+
+
+import { Link } from "react-router-dom";
+
+import Footer from "../Footer";
+
+
+
 
 const Blogs = ({ isDarkMode, toggleMode }) => {
   const [blogs, setBlog] = useState();
@@ -15,19 +17,13 @@ const Blogs = ({ isDarkMode, toggleMode }) => {
   useEffect(() => {
     const fetchBlogs = async () => {
       try {
-        const blogsRef = collection(db, "blogs");
-        const blogquery = query(blogsRef, orderBy("date", "desc"));
-        const blogsSnapshot = await getDocs(blogquery);
-        const blogsList = blogsSnapshot.docs.map((doc) => {
-          return {
-            id: doc.id,
-            ...doc.data(),
-          };
-        });
-
-        setBlog(blogsList);
+        const response = await db.listDocuments(cms_db_id, blog_collection_id);
+        setBlog(response.documents);
+        console.log(blogs)
       } catch (error) {
         console.error("Error fetching blogs:", error);
+        setError("Failed to fetch blogs. Please try again.");
+
       }
     };
 
@@ -37,16 +33,10 @@ const Blogs = ({ isDarkMode, toggleMode }) => {
   return (
     <div className={`h-fit ${isDarkMode ? "dark" : "light"} flex flex-col`}>
       <Nav toggleMode={toggleMode} isDarkMode={isDarkMode} />
-      {/* <div className="w-full justify-center flex text-1x+">
-        Subscribe to the newsletter
-      </div> */}
-      {/* <iframe
-        src="https://anshumancdx.substack.com/embed"
-        className="mt-5 bg-inherit"
-      ></iframe> */}
+    
 
       <section className="py-20">
-        <h1 className="mb-12 text-center font-sans text-5xl font-bold">
+        <h1 className="mb-12 text-center text-5xl font-bold">
           Recent Posts
         </h1>
         <div className="mx-auto grid max-w-screen-lg grid-cols-1 gap-5 p-5 sm:grid-cols-2 md:grid-cols-3 lg:gap-10">
@@ -58,10 +48,10 @@ const Blogs = ({ isDarkMode, toggleMode }) => {
                   <img src={blog.primaryimg} alt={blog.title} className="h-[90%] w-full object-cover" />
                 </div>
                 <div className="px-5">
-                  <p className="text-blue-500 font-bold text-lg mb-2">{blog.topic ? blog.topic : "Technology"}</p>
+                 
                   <h2 className="text-2xl mb-2">{blog.title}</h2>
-                  <p className="text-gray-600">{blog.intro ? blog.intro : "No Blog intro found"}</p>
-                  <a href={`/blog/${blog.id}`} className="text-blue-500 block mt-3">Read more</a>
+                  <p className="text-gray-600">{blog.content ?`${blog.content.split(200)}...`: "No Blog intro found"}</p>
+                  <a href={`/blog/${blog.$id}`} className="text-blue-500 block mt-3">Read more</a>
                 </div>
               </div>
             
