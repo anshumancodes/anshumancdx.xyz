99383639541d63535dd59f5ff8b5fe48cad4b275
anshumancodes <anshumanprof01@gmail.com>
2024-10-18 04:02:18 +0530

    update admin login function with appwrite and update redirect links

--- app/src/components/admin/Adminlogin.jsx
+++ app/src/components/admin/Adminlogin.jsx
@@ -1,64 +1,110 @@
-import React, { useState } from "react";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../../config/Firebase";
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from 'react';
+import { account, ID } from '../../config/appwriteconfig';
+import {useNavigate } from 'react-router-dom';
 
-const Adminlogin = ({ onLogin }) => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const navigate = useNavigate();
 
-  const handleLogin = async (e) => {
-    e.preventDefault();
+const AdminLogin = () => {
+  const [loggedInUser, setLoggedInUser] = useState(null);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
+  const navigate=useNavigate()
 
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-
-      //
-     
-
-      // Call the onLogin callback or perform other actions as needed
-      onLogin(true);
-      
-      navigate("/admin/createblog")
-      
-    } catch (error) {
-      // Handle login errors
-      console.error("Error logging in:", error.message);
-      // Display an error message or handle it as needed
-    }
-  };
+  async function login(email, password) {
+    await account.createEmailPasswordSession(email, password);
+    setLoggedInUser(await account.get());
+    setTimeout(() => {
+      navigate("/cms")
+    }, 3000);
+  }
 
   return (
-    <div className="bg-black-bg h-screen items-center flex flex-col justify-center">
-        {/*  */}
+    <div className="flex h-screen w-screen items-center overflow-hidden px-2">
+      {/* Login */}
+      
+      <div className="relative flex w-96 flex-col space-y-5 rounded-lg border bg-white px-5 py-10 shadow-xl sm:mx-auto">
+        <div className="-z-10 absolute top-4 left-1/2 h-full w-5/6 -translate-x-1/2 rounded-lg bg-blue-600 sm:-right-10 sm:top-auto sm:left-auto sm:w-full sm:translate-x-0"></div>
+        <div className="mx-auto mb-2 space-y-3">
+          <h1 className="text-center text-3xl font-bold text-gray-700">Sign in</h1>
+          {loggedInUser ? `Logged in as ${loggedInUser.name}` : 'Not logged in'}
+        </div>
 
-<h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-1x++ lg:text-2xl"><span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">login</span> to acess admin panel.</h1>
+        {/* Email Input */}
+        <div>
+          <div className="relative mt-2 w-full">
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pt-4 pb-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+              placeholder=" "
+            />
+            <label
+              htmlFor="email"
+              className="origin-[0] peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 absolute left-1 top-2 z-10 -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300"
+            >
+              Enter Your Email
+            </label>
+          </div>
+        </div>
 
-  {/*  */}
-      <form onSubmit={handleLogin} className="flex flex-col w-300pwx gap-4">
-        <label className="text-white-color">
-          Email:
-        
-        </label>
-        <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        <label className="text-white-color">
-          Password:
-          
-        </label>
-        <input  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 outline-none"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        <button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 mt-2" type="submit">Login</button>
-      </form>
+        {/* Password Input */}
+        <div>
+          <div className="relative mt-2 w-full">
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pt-4 pb-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+              placeholder=" "
+            />
+            <label
+              htmlFor="password"
+              className="origin-[0] peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 absolute left-1 top-2 z-10 -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300"
+            >
+              Enter Your Password
+            </label>
+          </div>
+        </div>
+
+        {/* Name Input */}
+        <div>
+          <div className="relative mt-2 w-full">
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="border-1 peer block w-full appearance-none rounded-lg border border-gray-300 bg-transparent px-2.5 pt-4 pb-2.5 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+              placeholder=" "
+            />
+            <label
+              htmlFor="name"
+              className="origin-[0] peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-blue-600 absolute left-1 top-2 z-10 -translate-y-4 scale-75 transform cursor-text select-none bg-white px-2 text-sm text-gray-500 duration-300"
+            >
+              Enter Your Name
+            </label>
+          </div>
+        </div>
+
+        {/* Login Button */}
+        <div className="flex w-full items-center">
+          <button
+            onClick={() => login(email, password)}
+            className="shrink-0 inline-block w-36 rounded-lg bg-blue-600 py-3 font-bold text-white"
+          >
+            Login
+          </button>
+        </div>
+
+        {/* Links */}
+       
+      </div>
+      {/* /Login */}
     </div>
   );
 };
 
-export default Adminlogin;
+export default AdminLogin;
