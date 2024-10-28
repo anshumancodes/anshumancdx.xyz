c8fc134f664ac56a17627f129d129091cfcda01c
anshumancodes <anshumanprof01@gmail.com>
2024-10-29 04:22:57 +0530

    add aria labels because lighthouse said so

--- app/src/components/Nav.jsx
+++ app/src/components/Nav.jsx
@@ -9,12 +9,12 @@ const Nav = () => {
     <nav className="flex items-center justify-around py-8 shadow-sm ">
       <div className="flex gap-4 font-semibold">
         <a href="/">Home</a>
-        <a href="/blog">Blog</a>
-        <a href="/business">Hire Me</a>
-        <a href="https://github.com/anshumancodes" className="underline">GitHub</a>
+        <a aria-label="blog page" href="/blog">Blog</a>
+        <a aria-label="for business" href="/business">Hire Me</a>
+        <a aria-label="github link" href="https://github.com/anshumancodes" className="underline">GitHub</a>
       </div>
       <div>
-        <button onClick={toggleDarkMode} className="focus:outline-none  rounded-lg p-1">
+        <button onClick={toggleDarkMode} className="focus:outline-none  rounded-lg p-1" name="darkmode/lightmode" aria-label="darkmode/lightmode">
           {isDarkMode ? <Sun size={24} /> : <Moon size={24} />}
         </button>
       </div>
