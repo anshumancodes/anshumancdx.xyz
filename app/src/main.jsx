67979edc94f4849731681e812e2a0604b62a0ac2
anshumancodes <anshumanprof01@gmail.com>
2025-04-02 03:34:01 +0530

    🚀 Moved Router to main.jsx for better performance
     - Wrapped App with <Router> in index.js to prevent unnecessary re-renders.
          - Improved routing efficiency by ensuring a single Router instance at the root level.
          - Enhanced maintainability by keeping routing logic centralized.

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { DarkModeProvider } from './context/DarkModeContext.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Router>
     <DarkModeProvider>
     <App />
     </DarkModeProvider>
+    </Router>
    
   </React.StrictMode>,
 )
